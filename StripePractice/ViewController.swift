684438a7b73deacc28546e1c94bdca74cde3169d
Sidharth <sidharth100895@gmail.com>
2022-01-14 17:15:54 -0500

    Initial Commit

---
+++ StripePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StripePractice
+//
+//  Created by Sidharth Mehta on 2022-01-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
